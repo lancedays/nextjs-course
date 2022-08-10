30cfb9ad7cf08a9bc0b2b52c40b34c96bab094db
Lance Days <lancedays@gmail.com>
2022-08-10 09:04:17 -0400

    Add nested dynamic routes for client

---
+++ pages/clients/[id]/index.js
@@ -0,0 +1,7 @@
+export default function ClientProjectsPage() {
+  return (
+    <div>
+      <h1>This is the clients project list page</h1>
+    </div>
+  );
+}
---
+++ pages/clients/index.js
@@ -0,0 +1,7 @@
+export default function ClientsPage() {
+  return (
+    <div>
+      <h1>This is the clients page</h1>
+    </div>
+  );
+}
