c9d5c50f23f2cf41ead2449ba2a72ce08bc668b1
Lance Days <lancedays@gmail.com>
2022-08-10 08:42:15 -0400

    Add about page, change default exports

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
-function HomePage() {
-  return <div>This is a test</div>;
+export default function HomePage() {
+  return (
+    <div>
+      <h1>This is the home page</h1>
+    </div>
+  );
 }
-
-export default HomePage;
