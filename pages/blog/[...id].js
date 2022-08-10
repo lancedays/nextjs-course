5f7ed6e766024f8e90bacc99e9b70c220d7bb811
Lance Days <lancedays@gmail.com>
2022-08-10 09:11:48 -0400

    Add catch all route to blog page

---
+++ pages/blog/[...id].js
@@ -0,0 +1,10 @@
+import { useRouter } from "next/router";
+export default function BlogDetailPage() {
+  const router = useRouter();
+
+  return (
+    <div>
+      <h1>This is the blog detail page</h1>
+    </div>
+  );
+}
