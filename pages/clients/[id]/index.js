02703feab0ea5a79dfb018cabbb911b7a9a0a6fc
Lance Days <lancedays@gmail.com>
2022-08-10 10:52:11 -0400

    Add links for dynamic routes using Link component

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,7 +1,13 @@
+import { useRouter } from "next/router";
+
 export default function ClientProjectsPage() {
+  const router = useRouter();
+  var clientName = router.query.id;
+  console.log(clientName);
   return (
     <div>
       <h1>This is the clients project list page</h1>
+      <div>The client that you selected is {clientName}</div>
     </div>
   );
 }
--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,24 @@
+import Link from "next/link";
+
 export default function ClientsPage() {
+  const clientList = [
+    { id: "lancedays", name: "Lance Days" },
+    { id: "daniel", name: "Daniel" },
+    { id: "max1233", name: "Max" },
+  ];
+
   return (
     <div>
       <h1>This is the clients page</h1>
+      <ul>
+        {clientList.map((client) => {
+          return (
+            <li key={client.id}>
+              <Link href={`/clients/${client.id}`}>{client.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
