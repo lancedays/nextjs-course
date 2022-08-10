d23d82e61785834a137f9c18e9e19f4282e996dc
Lance Days <lancedays@gmail.com>
2022-08-10 11:04:02 -0400

    Add example of programmatic routing

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -4,10 +4,17 @@ export default function ClientProjectsPage() {
   const router = useRouter();
   var clientName = router.query.id;
   console.log(clientName);
+
+  function loadProjectHandler() {
+    // Load data...
+    router.push(`/clients/${clientName}/projecta`);
+  }
+
   return (
     <div>
       <h1>This is the clients project list page</h1>
       <div>The client that you selected is {clientName}</div>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
