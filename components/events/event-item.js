515446b26b6305a91a0b2ebeec18b04f56a6d5e1
Lance Days <lancedays@gmail.com>
2022-08-12 09:37:41 -0400

    Add event-list component, event-item component

---
+++ components/events/event-item.js
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+export default function EventItem(props) {
+  const { title, image, date, location, id } = props;
+  const hrDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+  const formattedAddress = location.replace(", ", "\n");
+  return (
+    <li>
+      <img src={"/" + image} alt={title} />
+      <div>
+        <div>
+          <h2>{title}</h2>
+          <div>
+            <time>{hrDate}</time>
+          </div>
+          <div>
+            <address>{formattedAddress}</address>
+          </div>
+        </div>
+        <div>
+          <Link href={`/events/${id}`}>Explore Event</Link>
+        </div>
+      </div>
+    </li>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import EventList from "../components/events/event-list";
+import { getFeaturedEvents } from "../dummy-data";
+
 export default function HomePage() {
+  const featuredEvents = getFeaturedEvents();
+  console.log(featuredEvents);
   return (
     <div>
-      <h1>This is the Home page</h1>
+      <EventList events={featuredEvents} />
     </div>
   );
 }
