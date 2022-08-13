7e6bbb44b84e052a1496491d0aecfed8ff08cb1a
Lance Days <lancedays@gmail.com>
2022-08-13 08:45:56 -0400

    Add event search component and handlers

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,22 @@
+import { Fragment } from "react";
+import { useRouter } from "next/router";
 import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
 import { getAllEvents } from "../../dummy-data";
+
 export default function EventsPage() {
+  const router = useRouter();
   const events = getAllEvents();
+
+  function findEventsHandler(year, month) {
+    const path = `/events/${year}/${month}`;
+    router.push(path);
+  }
+
   return (
-    <div>
+    <Fragment>
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList events={events} listTitle="All Events" />
-    </div>
+    </Fragment>
   );
 }
