3b41b33543741757981756684f61a1b080a6d5a5
Lance Days <lancedays@gmail.com>
2022-08-13 09:02:24 -0400

    Add logic to filter events using a slug

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,7 +1,38 @@
+import { getFilteredEvents } from "../../dummy-data";
+import { useRouter } from "next/router";
+import EventList from "../../components/events/event-list";
+
 export default function FilteredEventsPage() {
+  const router = useRouter();
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const year = +filterData[0];
+  const month = +filterData[1];
+
+  if (
+    isNaN(year) ||
+    isNaN(month) ||
+    year > 2030 ||
+    year < 2021 ||
+    month < 1 ||
+    month > 12
+  ) {
+    return <p className="center">Invalid Filter</p>;
+  }
+
+  const events = getFilteredEvents({ year: year, month: month });
+
+  if (!events || events.length === 0) {
+    return <p className="center">No events found</p>;
+  }
+
   return (
     <div>
-      <h1>This is the Filtered Events Page</h1>
+      <EventList events={events} listTitle="Event Search" />
     </div>
   );
 }
--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -9,6 +9,7 @@ export default function EventDetailPage() {
   const router = useRouter();
   const eventId = router.query.eventId;
   const event = getEventById(eventId);
+
   if (!event) {
     return <p>No event found!</p>;
   }
