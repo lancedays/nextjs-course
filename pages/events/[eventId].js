ec881dbde145c340f4d0a4574f01fc45e14b9843
Lance Days <lance.days@jax.org>
2022-08-18 13:00:15 -0400

    Optimize code on featuredEvents page and eventDetail page

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { getEventById, getAllEvents } from "../../helpers/api-util";
+import { getEventById, getFeaturedEvents } from "../../helpers/api-util";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
@@ -12,9 +12,9 @@ export default function EventDetailPage(props) {
   if (!event) {
     return (
       <Fragment>
-        <ErrorAlert>
-          <p>No event found!</p>
-        </ErrorAlert>
+        <div className="center">
+          <p>Loading...</p>
+        </div>
         <div className="center">
           <Button link="/events">Show All Events</Button>
         </div>
@@ -45,14 +45,15 @@ export async function getStaticProps(context) {
     props: {
       event: event,
     },
+    revalidate: 30,
   };
 }
 
 export async function getStaticPaths() {
-  const events = await getAllEvents();
+  const events = await getFeaturedEvents();
   const paths = events.map((event) => ({ params: { eventId: event.id } }));
   return {
     paths: paths,
-    fallback: false,
+    fallback: true,
   };
 }
--- pages/index.js
+++ pages/index.js
@@ -18,5 +18,6 @@ export async function getStaticProps(context) {
     props: {
       featuredEvents: featuredEvents,
     },
+    revalidate: 1800,
   };
 }
