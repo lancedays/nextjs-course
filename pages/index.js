1ef02730fe5fc304d24139c23b0cb73fc8b6f6a6
Lance Days <lance.days@jax.org>
2022-08-25 09:16:23 -0400

    refactor(client): update featured events to use new events API routes

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,24 @@
 import { getFeaturedEvents } from "../helpers/api-util";
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import Head from "next/head";
 import EventList from "../components/events/event-list";
 import ResultsTitle from "../components/events/results-title";
 import NewsletterRegistration from "../components/input/newsletter-registration";
 
 export default function HomePage(props) {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    if (events) {
+      fetch("/api/events")
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          setEvents(data.events);
+        });
+    }
+  }, []);
+
   return (
     <Fragment>
       <Head>
@@ -13,17 +26,7 @@ export default function HomePage(props) {
       </Head>
       <ResultsTitle pageTitle="Featured Events" />
       <NewsletterRegistration />
-      <EventList events={props.featuredEvents} />
+      <EventList events={events} />
     </Fragment>
   );
-}
-
-export async function getStaticProps(context) {
-  const featuredEvents = await getFeaturedEvents();
-  return {
-    props: {
-      featuredEvents: featuredEvents,
-    },
-    revalidate: 60,
-  };
 }
