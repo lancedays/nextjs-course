e730e49128384ae0168f0708ca37b969558742da
Lance Days <lance.days@jax.org>
2022-08-25 09:48:51 -0400

    refactor(event detail): Update event detail to use new events API routes

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,5 +1,5 @@
-import { Fragment } from "react";
-import { getEventById, getFeaturedEvents } from "../../helpers/api-util";
+import { Fragment, useEffect, useState } from "react";
+import { useRouter } from "next/router";
 import Head from "next/head";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
@@ -7,8 +7,20 @@ import EventContent from "../../components/event-detail/event-content";
 import Button from "../../components/ui/button.js";
 import Comments from "../../components/input/comments";
 
-export default function EventDetailPage(props) {
-  const { event } = props;
+export default function EventDetailPage() {
+  const router = useRouter();
+  const [event, setEvent] = useState();
+  const eventId = router.query.eventId;
+
+  useEffect(() => {
+    if (eventId) {
+      fetch(`/api/events/${eventId}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setEvent(data.event);
+        });
+    }
+  }, [router.query]);
 
   if (!event) {
     return (
@@ -44,24 +56,4 @@ export default function EventDetailPage(props) {
       <Comments eventId={event.id} />
     </Fragment>
   );
-}
-
-export async function getStaticProps(context) {
-  const eventId = context.params.eventId;
-  const event = await getEventById(eventId);
-  return {
-    props: {
-      event: event,
-    },
-    revalidate: 30,
-  };
-}
-
-export async function getStaticPaths() {
-  const events = await getFeaturedEvents();
-  const paths = events.map((event) => ({ params: { eventId: event.id } }));
-  return {
-    paths: paths,
-    fallback: true,
-  };
 }
